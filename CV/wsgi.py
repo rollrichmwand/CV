ea99302068d8bca5301cd9997521d9a8b6d274bf
rollrichmwand <rollrichmwand@gmail.com>
2026-01-13 12:21:25 +0200

    Add CV files

--- CV/wsgi.py
+++ CV/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+
+path = '/home/Rollrichmwand5/CV'
+if path not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CV.settings')
