832b824d4c4b8762a16908b169bb6caf0bc0ca76
rollrichmwand <rollrichmwand@gmail.com>
2026-01-13 12:39:48 +0200

    Fix ALLOWED_HOSTS

--- CV/settings.py
+++ CV/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g%-!ai8_vw$c^_u3t+d$r$krjg8f##56col#*grfs89=#y-42o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['rollrichcv2.onrender.com']
-CSRF_TRUSTED_ORIGINS =[ 'https://rollrichcv2.onrender.com']
+ALLOWED_HOSTS = ['rollrichmwand5.pythonanywhere.com']
+# CSRF_TRUSTED_ORIGINS =[ 'https://rollrichcv2.onrender.com']
 
 # Application definition
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
